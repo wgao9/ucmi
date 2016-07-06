39030c1c2d91be069ad870c075f0f2d269e760b0
Weihao Gao <liverlover001@gmail.com>
2016-07-06 11:43:57 -0500

    Add files via upload

--- demo.py
+++ demo.py
@@ -1,14 +1,24 @@
 import numpy.random as nr
 import ucmi 
 
-def main():
+def demo1():
+	print "Demo 1 : Joint Gaussian"
 	x = nr.normal(0,1,[1000,1])
 	y = nr.normal(0,1,[1000,1])
 	print "I(X;Y) = ", ucmi.mi(x,y)
 	print "UMI(X;Y) = ", ucmi.umi(x,y)
 	print "CMI(X;Y) = ", ucmi.cmi(x,y)
 
+def demo2():
+	print "Demo2 : Beta + Gaussian Noise"
+	x = nr.beta(1.5,1.5,[1000,1])
+	y = x + nr.normal(0,0.3,[1000,1])
+	print "I(X;Y) = ", ucmi.mi(x,y)
+	print "UMI(X;Y) = ", ucmi.umi(x,y)
+	print "CMI(X;Y) = ", ucmi.cmi(x,y)
+
 if __name__ == '__main__':
-	main()
+	demo1()
+	demo2()
 
 
